f2f8d1dfa3e2e96895179b3d58dd345cf1340ea7
Pacome Schmitt <pacschmitt@gmail.com>
2022-11-28 16:26:18 -0600

    created the user controller, updated the view gears

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:show, :edit, :update]
-  resources :gears, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :users, only: [:show, :edit, :update] do
+    resources :gears, only: [:index]
+  end
+  resources :gears, except: :index
   # Defines the root path route ("/")
   # root "articles#index"
 end
