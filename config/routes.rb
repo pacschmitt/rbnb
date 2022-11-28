7a1a48ba98aa1e62eec21ab0947bb17cabdc4374
Pacome Schmitt <pacschmitt@gmail.com>
2022-11-28 12:35:30 -0600

    updated migrations

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :users, only: [:show, :edit, :update]
+  resources :gears, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   # Defines the root path route ("/")
   # root "articles#index"
 end
