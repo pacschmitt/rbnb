f7cbff3b6018d844de435eb4f12a31f375d9788a
Luis angel padilla sanchez <areadedocupast@gmail.com>
2022-11-28 12:50:42 -0600

    controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_28_173246) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_28_183005) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pages", force: :cascade do |t|
+    t.string "about"
+    t.string "html"
+    t.string "erb"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
