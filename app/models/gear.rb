b4c3de60c7e722292201681b12fbf88076eecbc4
Pacome Schmitt <pacschmitt@gmail.com>
2022-11-29 17:54:37 -0600

    added photos caards profile, cloudify gem updated controllers and storage yml

--- app/models/gear.rb
+++ app/models/gear.rb
@@ -1,6 +1,8 @@
 class Gear < ApplicationRecord
+  CATEGORIES = %w[Cycling Outdoors Racket-Sports Team-Sports Winter-Sports Dance Exercice Golf Precision-Sports Skates&Skateboards]
   belongs_to :user
   has_many :bookings, dependent: :destroy
+  has_one_attached :photo
 
   validates :name, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :bookings
   has_many :gears
+  has_one_attached :photo
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
