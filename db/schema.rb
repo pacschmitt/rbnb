ac75d1d22cf13e211f112e2e0415b253e2207ed5
Pacome Schmitt <pacschmitt@gmail.com>
2022-12-01 15:06:34 -0600

    lets have conflics

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_30_165517) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_01_210003) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,6 +65,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_30_165517) do
     t.float "longitude"
     t.string "address"
     t.index ["user_id"], name: "index_gears_on_user_id"
+  end
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.bigint "searchable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable"
   end
 
   create_table "users", force: :cascade do |t|
