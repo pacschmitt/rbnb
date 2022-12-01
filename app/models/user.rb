df67c5e9db84172b95b83fcdc49bf2eee85f5e3a
Alfonso Nava <navafe.ga@gmail.com>
2022-12-01 12:05:05 -0600

    fixing conflicts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
-  include PgSearch::Model
+  # include PgSearch::Model
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :bookings, dependent: :destroy
   has_many :gears, dependent: :destroy
   has_one_attached :photo
 
-  multisearchable against: [ :name, :description, :address ]
+  # multisearchable against: [ :name, :description, :address ]
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
