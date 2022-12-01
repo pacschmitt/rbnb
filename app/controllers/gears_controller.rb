d0e58b594bba00bd28d7bb56f36a1bc39112cf51
Pacome Schmitt <pacschmitt@gmail.com>
2022-12-01 10:33:33 -0600

    a lot of stuff

--- app/controllers/gears_controller.rb
+++ app/controllers/gears_controller.rb
@@ -5,6 +5,20 @@ class GearsController < ApplicationController
   def index
     @gears = Gear.all
     @gears = policy_scope(Gear)
+    if params[:query].present?
+      # sql_query = <<~SQL
+      #   gears.name @@ :query
+      #   OR gears.description @@ :query
+      #   OR gears.category @@ :query
+      #   OR gears.address @@ :query
+      #   OR users.first_name @@ :query
+      #   OR users.last_name @@ :query
+      # SQL
+      # @gears = Gear.joins(:user).where(sql_query, query: "%#{params[:query]}%")
+      @gears = Gear.global_search(params[:query])
+    else
+      @gears = Gear.all
+    end
     @markers = @gears.geocoded.map do |gear|
       {
         lat: gear.latitude,
@@ -15,6 +29,7 @@ class GearsController < ApplicationController
 
   def show
     authorize @gear
+    image_bg
   end
 
   def new
@@ -28,6 +43,7 @@ class GearsController < ApplicationController
     authorize @gear
     if @gear.save
       redirect_to gear_path(@gear)
+      flash.alert = "You've created a new Gear"
     else
       render :new, status: :unprocessable_entity
     end
@@ -41,6 +57,7 @@ class GearsController < ApplicationController
     authorize @gear
     if @gear.update(gear_params)
       redirect_to gear_path(@gear)
+      flash.alert = "You've Updated your post: #{@gear.name}"
     else
       render :edit, status: :unprocessable_entity
     end
@@ -49,7 +66,8 @@ class GearsController < ApplicationController
   def destroy
     authorize @gear
     @gear.destroy
-    redirect_to user_gears_path(current_user)
+    redirect_to user_path(current_user)
+    flash.alert = "You've Deleted your #{@gear.name}"
   end
 
   private
@@ -60,5 +78,14 @@ class GearsController < ApplicationController
 
   def set_gear
     @gear = Gear.find(params[:id])
+  end
+
+  def image_bg
+    # image = (cl_image_tag "dwl90cghmju94tsyc7b0z07wlbmq", width: 700, height: 400, crop: :scale)
+    if @gear.category == "cycling"
+      @image = "https://res.cloudinary.com/df2jp0y94/image/upload/v1669389420/cld-sample-2.jpg"
+    else
+      @image = "https://res.cloudinary.com/df2jp0y94/image/upload/c_crop,h_400,w_1506,x_0,y_550/v1669861095/photo-1471506480208-91b3a4cc78be_qcnsvw.jpg"
+    end
   end
 end
