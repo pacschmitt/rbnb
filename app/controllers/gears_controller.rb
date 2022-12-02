f58aa859cf39e3907494cae7471a4c53232e7557
Pacome Schmitt <pacschmitt@gmail.com>
2022-12-02 12:16:41 -0600

    pull

--- app/controllers/gears_controller.rb
+++ app/controllers/gears_controller.rb
@@ -5,24 +5,22 @@ class GearsController < ApplicationController
   def index
     @gears = Gear.all
     @gears = policy_scope(Gear)
+
     if params[:query].present?
-      # unless Gear.near(params[:query], 20).nil?
-      #   @gears = Gear.near(params[:query], 20)
-      # else
+      if params[:address].present?
+        selection = Gear.near(params[:address], 20)
+        @gears = selection.global_search(params[:query]) if params[:query].present?
+      else
         @gears = Gear.global_search(params[:query])
-      #end
-      # sql_query = <<~SQL
-      #   gears.name @@ :query
-      #   OR gears.description @@ :query
-      #   OR gears.category @@ :query
-      #   OR gears.address @@ :query
-      #   OR users.first_name @@ :query
-      #   OR users.last_name @@ :query
-      # SQL
-      # @gears = Gear.joins(:user).where(sql_query, query: "%#{params[:query]}%")
+      end
     else
-      @gears = Gear.all
+      if params[:address].present?
+        @gears = Gear.near(params[:address], 20)
+      else
+        @gears = Gear.all
+      end
     end
+
     @markers = @gears.geocoded.map do |gear|
       {
         lat: gear.latitude,
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -6,15 +6,19 @@ class Booking < ApplicationRecord
   validates :end_date, presence: true
   validate :no_booking_overlap
 
-  scope :overlapping, ->(start_date, end_date) do
-    where "((start_date <= ?) and (end_date >= ?))", end_date, start_date
-  end
-
   private
 
   def no_booking_overlap
-    if Booking.overlapping(start_date, end_date).any?
-      errors.add(:end_date, 'it overlaps another reservation')
+    gear_booking = []
+    Booking.all.each do |booking|
+      if booking.gear == gear
+        gear_booking << booking
+      end
+    end
+    gear_booking.each do |booking|
+      if (start_date <= booking.end_date) && (end_date >= booking.start_date)
+        errors.add(:end_date, 'it overlaps another reservation')
+      end
     end
   end
 end
