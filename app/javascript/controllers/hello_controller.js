cd9257efffb34171a8992d1078d16942d0206ebb
Alfonso Nava <navafe.ga@gmail.com>
2022-12-02 12:07:37 -0600

    fixed

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,12 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "bookings", "gears"]
+  static targets = [ "bookings", "gears", "button", "xbutton"]
   show() {
     this.bookingsTarget.classList.toggle("d-none")
+    this.buttonTarget.classList.toggle("active")
   }
   display() {
     this.gearsTarget.classList.toggle("d-none")
+    this.xbuttonTarget.classList.toggle("active")
   }
 
 }
